1674da94fd124ec31daf67dfa4a04772f9d44b95
rubber-bird <marko.turchyn@gmail.com>
2021-07-01 17:09:38 -0400

    Added index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import express from 'express';
-import morgan from 'morgan';
+// import morgan from 'morgan';
 
 import defaults from './routes/defaults';
 import products from './routes/products';
 
 const app = express();
 
-const PORT = 3000;
-
 app.use(express.json());
-app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
+// app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
 
 app.use('/', defaults);
 app.use('/products', products);
 
-app.listen(PORT, () => {
-  console.log(`This server is running at localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`This server is running at localhost:${process.env.PORT}`);
 });
 
 export default app;
